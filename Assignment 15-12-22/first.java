140a19050a6b2a2613458b816436ab1e7a466573
bivor017 <112678635+bivor017@users.noreply.github.com>
2022-12-15 12:28:29 +0530

    Create first.java

---
+++ Assignment 15-12-22/first.java
@@ -0,0 +1,20 @@
+//WAP to print hello 5 times and world 5 times.
+
+package basic;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		int i ;
+		for(i=0; i<5; i++) {
+			System.out.println(i +"Hello");
+		}
+		System.out.println();
+		for(i=5;i>0;i--) {
+			System.out.println(i +"World");
+		}
+
+	}
+
+}
