2c96af35bc2fa8f6ed5abc6584e55d27f5d51a65
bivor017 <112678635+bivor017@users.noreply.github.com>
2022-12-16 12:26:09 +0530

    Create NestedForLoop.java

---
+++ Assignment 16-12-2022/NestedForLoop.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		// loop for i
+		
+		for(int i =1; i<=3;i++) {
+			
+			//loop for j
+			for(int j=1;j<=3;j++) {
+				System.out.println(i+" "+j);
+			}
+		}
+
+	}
+
+}
