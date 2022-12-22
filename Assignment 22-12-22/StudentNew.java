f4af7cec6ddf41a754eb4334514e7c140441ef57
bivor017 <112678635+bivor017@users.noreply.github.com>
2022-12-22 13:42:13 +0530

    Create StudentNew.java

---
+++ Assignment 22-12-22/StudentNew.java
@@ -0,0 +1,52 @@
+package Oops;
+
+public class StudentNew{
+	private int id;
+	private String firstName,lastName,address;
+	
+	
+	public StudentNew() {
+		super();
+	}
+	
+	public StudentNew(int id, String firstName, String lastName, String address) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
